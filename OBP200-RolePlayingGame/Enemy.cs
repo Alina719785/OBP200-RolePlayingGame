e4e6a10e7696b5b606a25a6682208f186de1064e
arijb <alba0033@student.hv.se>
2026-03-28 19:08:44 +0100

    Skapade Enemy och Boss klasser med arv.

---
+++ OBP200-RolePlayingGame/Enemy.cs
@@ -0,0 +1,22 @@
+﻿namespace OBP200_RolePlayingGame;
+
+public class Enemy
+{
+    public string Name { get; set; }
+    public int HP { get; set; }
+    public int ATK { get; set; }
+    public int DEF { get; set; }
+
+    public virtual int Attack()
+    {
+        return ATK;
+    }
+}
+
+public class Boss : Enemy
+{
+    public override int Attack()
+    {
+        return ATK + 3; 
+    }
+}
